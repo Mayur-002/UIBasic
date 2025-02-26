073ae9673f8a8ad7dc5ec501e733201804393e2e
Your Name <your.email@example.com>
2025-02-26 13:50:38 +0530

    Ui basics

---
+++ js/myscripts.js
@@ -0,0 +1,5 @@
+function addoperation(){
+  var firstNovalue=Number(document.getElementById('firstNo').value)
+  var secondNovalue=Number(document.getElementById('secondNo').value)
+  console.log(firstNovalue+secondNovalue)
+}
